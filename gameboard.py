5c3da7f72505900fc96a4b1e3f90d866d40c2051
Webtron18 <jmasse@webtron.me>
2018-05-19 11:42:56 -0400

    Added gui elements from tkinter

--- gameboard.py
+++ gameboard.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from tkinter import *
 
 class GameBoard():
     
@@ -23,3 +24,32 @@ class GameBoard():
     
     def printObj(self, ship):
         print(ship.getSize())
+        
+        
+class DrawBoard:
+    
+    def __init__(self,board,size):
+        #self.ships = Button(board, text="Ship1")
+        #self.ships.grid(columnspan=2)
+        self.frames = []
+        self.ctr = 0
+        for x in range(0,size):
+            self.frames.append([])
+            for y in range(0,size):
+                self.frames[x].append(Frame(board, width=20, height=20, bg="blue"))
+                self.frames[x][y].grid(row=x+2, column=y)
+                self.frames[x][y].bind("<Enter>", self.on_enter)
+                self.frames[x][y].bind("<Leave>", self.on_leave)
+                self.frames[x][y].bind("<Button-1>", self.mouse_click)
+                
+    def on_enter(self, event):
+        if(event.widget.cget('bg') != 'purple'):
+            event.widget.configure(bg='red')
+        
+    def on_leave(self, event):
+        if(event.widget.cget('bg') != 'purple'):
+            event.widget.configure(bg='blue')
+    
+    def mouse_click(self, event):
+        event.widget.configure(bg='purple')
+        
